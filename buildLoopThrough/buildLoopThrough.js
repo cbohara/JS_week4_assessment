e1a751db8a8ce34978d7ab63f01cb47ee8e4e4a9
Albrey Brown <albrey.brown@gmail.com>
2016-02-13 09:50:51 -0800

    fixed loop through instructions

--- buildLoopThrough/buildLoopThrough.js
+++ buildLoopThrough/buildLoopThrough.js
@@ -1,15 +1,17 @@
 /*
 
-sumArray should take an array of numbers and return the sum of each number.
+Build a loopthrough function that will loop through an array and invoke a callback on each element.
 
-example: 
+var arr = [1, 2, 3, 4, 5];
+var print = function(val){
+	console.log(val);
+};
 
-var arr = [1, 9, 7, 15];
-
-sumArray(arr) --> 32;
-
-
-*EXTRA* build an 'add' function that takes two numbers as parameters and adds them together. Replace your add functionality inside of sumArray with your new 'add' function.
-
+loopThrough(arr, print)
+//1
+//2
+//3
+//4
+//5
 */
 
